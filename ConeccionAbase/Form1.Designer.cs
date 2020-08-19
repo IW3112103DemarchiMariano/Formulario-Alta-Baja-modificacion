8bfb501c85edcd92c38e6aacb2458145e470076b
IW3112103DemarchiMariano <69605977+IW3112103DemarchiMariano@users.noreply.github.com>
2020-08-19 19:31:47 +0200

    Initial commit

---
+++ ConeccionAbase/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace ConeccionAbase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtDistribuidora = new System.Windows.Forms.TextBox();
+            this.lblDistribuidora = new System.Windows.Forms.Label();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.txtUnidades = new System.Windows.Forms.TextBox();
+            this.lblCant = new System.Windows.Forms.Label();
+            this.ListaData = new System.Windows.Forms.DataGridView();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.cboProductos = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnListar = new System.Windows.Forms.Button();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ListaData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(3, 543);
+            this.btnConectar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(100, 30);
+            this.btnConectar.TabIndex = 4;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(144, 120);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(132, 23);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(16, 120);
+            this.lblNombre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(56, 17);
+            this.lblNombre.TabIndex = 6;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // txtDistribuidora
+            // 
+            this.txtDistribuidora.Location = new System.Drawing.Point(144, 547);
+            this.txtDistribuidora.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtDistribuidora.Name = "txtDistribuidora";
+            this.txtDistribuidora.Size = new System.Drawing.Size(132, 23);
+            this.txtDistribuidora.TabIndex = 7;
+            // 
+            // lblDistribuidora
+            // 
+            this.lblDistribuidora.AutoSize = true;
+            this.lblDistribuidora.Location = new System.Drawing.Point(13, 197);
+            this.lblDistribuidora.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDistribuidora.Name = "lblDistribuidora";
+            this.lblDistribuidora.Size = new System.Drawing.Size(89, 17);
+            this.lblDistribuidora.TabIndex = 8;
+            this.lblDistribuidora.Text = "Distribuidora";
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(144, 272);
+            this.txtPrecio.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(132, 23);
+            this.txtPrecio.TabIndex = 9;
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Font = new System.Drawing.Font("Times New Roman", 10.25F, System.Drawing.FontStyle.Bold);
+            this.lblPrecio.Location = new System.Drawing.Point(16, 272);
+            this.lblPrecio.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(46, 17);
+            this.lblPrecio.TabIndex = 10;
+            this.lblPrecio.Text = "Precio";
+            // 
+            // txtUnidades
+            // 
+            this.txtUnidades.Location = new System.Drawing.Point(144, 339);
+            this.txtUnidades.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtUnidades.Name = "txtUnidades";
+            this.txtUnidades.Size = new System.Drawing.Size(132, 23);
+            this.txtUnidades.TabIndex = 11;
+            // 
+            // lblCant
+            // 
+            this.lblCant.AutoSize = true;
+            this.lblCant.Location = new System.Drawing.Point(13, 339);
+            this.lblCant.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCant.Name = "lblCant";
+            this.lblCant.Size = new System.Drawing.Size(63, 17);
+            this.lblCant.TabIndex = 12;
+            this.lblCant.Text = "Unidades";
+            // 
+            // ListaData
+            // 
+            this.ListaData.AllowUserToOrderColumns = true;
+            this.ListaData.BackgroundColor = System.Drawing.Color.OrangeRed;
+            this.ListaData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ListaData.Location = new System.Drawing.Point(319, 51);
+            this.ListaData.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ListaData.Name = "ListaData";
+            this.ListaData.Size = new System.Drawing.Size(498, 379);
+            this.ListaData.TabIndex = 13;
+            // 
+            // txtId
+            // 
+            this.txtId.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtId.Location = new System.Drawing.Point(144, 48);
+            this.txtId.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(132, 23);
+            this.txtId.TabIndex = 14;
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(165, 454);
+            this.btnMostrar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(100, 30);
+            this.btnMostrar.TabIndex = 15;
+            this.btnMostrar.Text = "Buscar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // cboProductos
+            // 
+            this.cboProductos.FormattingEnabled = true;
+            this.cboProductos.Location = new System.Drawing.Point(144, 197);
+            this.cboProductos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cboProductos.Name = "cboProductos";
+            this.cboProductos.Size = new System.Drawing.Size(132, 25);
+            this.cboProductos.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 51);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 17);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "lblIdProd";
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.BackColor = System.Drawing.SystemColors.GrayText;
+            this.btnBorrar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnBorrar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnBorrar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnBorrar.Image = global::ConeccionAbase.Properties.Resources.remove;
+            this.btnBorrar.Location = new System.Drawing.Point(319, 513);
+            this.btnBorrar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(75, 60);
+            this.btnBorrar.TabIndex = 3;
+            this.btnBorrar.UseVisualStyleBackColor = false;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnModificar.Image = global::ConeccionAbase.Properties.Resources.reload;
+            this.btnModificar.Location = new System.Drawing.Point(452, 510);
+            this.btnModificar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(75, 65);
+            this.btnModificar.TabIndex = 2;
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnListar
+            // 
+            this.btnListar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnListar.Image = global::ConeccionAbase.Properties.Resources.table;
+            this.btnListar.Location = new System.Drawing.Point(595, 510);
+            this.btnListar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(75, 65);
+            this.btnListar.TabIndex = 1;
+            this.btnListar.UseVisualStyleBackColor = true;
+            this.btnListar.Click += new System.EventHandler(this.btnListar_Click);
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnInsertar.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnInsertar.Image = global::ConeccionAbase.Properties.Resources.insert;
+            this.btnInsertar.Location = new System.Drawing.Point(742, 508);
+            this.btnInsertar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(75, 65);
+            this.btnInsertar.TabIndex = 0;
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(870, 588);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboProductos);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.ListaData);
+            this.Controls.Add(this.lblCant);
+            this.Controls.Add(this.txtUnidades);
+            this.Controls.Add(this.lblPrecio);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.lblDistribuidora);
+            this.Controls.Add(this.txtDistribuidora);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnListar);
+            this.Controls.Add(this.btnInsertar);
+            this.Font = new System.Drawing.Font("Times New Roman", 10.25F, System.Drawing.FontStyle.Bold);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ListaData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.Button btnListar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtDistribuidora;
+        private System.Windows.Forms.Label lblDistribuidora;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.TextBox txtUnidades;
+        private System.Windows.Forms.Label lblCant;
+        private System.Windows.Forms.DataGridView ListaData;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.ComboBox cboProductos;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ConeccionAbase/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConeccionAbase
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conexion = new SqlConnection("server=DESKTOP-A56APQQ\\SQLEXPRESS; database=Despensa; integrated security = true ");
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+            SqlCommand commando = new SqlCommand("Select  distinct distribuidora from Productos", conexion);
+            conexion.Open();
+            SqlDataReader registro = commando.ExecuteReader();
+            while (registro.Read())
+            {
+                cboProductos.Items.Add(registro["distribuidora"].ToString());
+            }
+            conexion.Close();
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            SqlCommand comando = new SqlCommand("Select * from Productos", conexion);
+            SqlDataAdapter adaptador = new SqlDataAdapter();
+            adaptador.SelectCommand = comando;
+            DataTable tabla = new DataTable();
+            adaptador.Fill(tabla);
+            ListaData.DataSource = tabla;
+
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                conexion.Open();
+                MessageBox.Show("Conectado");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error");
+            }
+
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            string query = "insert into Productos (nombre,distribuidora,precio,cantidad)VALUES (@nombre,@distribuidora,@precio,@cantidad)";
+            conexion.Open();
+            SqlCommand comando = new SqlCommand(query, conexion);
+            
+            comando.Parameters.AddWithValue("@nombre", txtNombre.Text);
+            comando.Parameters.AddWithValue("@distribuidora", cboProductos.Text);
+            comando.Parameters.AddWithValue("@precio", txtPrecio.Text);
+            comando.Parameters.AddWithValue("@cantidad", txtUnidades.Text);
+            comando.ExecuteNonQuery();
+            conexion.Close();
+            MessageBox.Show("Insertado");
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            string query = "UPDATE Productos set nombre = @nombre, distribuidora= @distribuidora, precio= @precio, cantidad= @cantidad where id_producto = @id_producto";
+            conexion.Open();
+            SqlCommand comando = new SqlCommand(query, conexion);
+            comando.Parameters.AddWithValue("@id_producto", txtId.Text);
+
+            comando.Parameters.AddWithValue("@nombre", txtNombre.Text);
+            comando.Parameters.AddWithValue("@distribuidora", cboProductos.Text);
+            comando.Parameters.AddWithValue("@precio", txtPrecio.Text);
+            comando.Parameters.AddWithValue("@cantidad", txtUnidades.Text);
+
+            comando.ExecuteNonQuery();
+            conexion.Close();
+            MessageBox.Show("Modificado");
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            string query = "DELETE from Productos where id_producto=@id_producto";
+            conexion.Open();
+            SqlCommand comando = new SqlCommand(query, conexion);
+            comando.Parameters.AddWithValue("@id_producto", txtId.Text);
+            comando.ExecuteNonQuery();
+            conexion.Close();
+            MessageBox.Show("Borrado");
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            SqlCommand comando = new SqlCommand("Select * from Productos where id_producto = @id_producto", conexion);
+            comando.Parameters.AddWithValue("@id_producto", txtId.Text);
+            conexion.Open();
+            SqlDataReader registro = comando.ExecuteReader();
+            if (registro.Read())
+            {
+                txtNombre.Text = registro["nombre"].ToString();
+                cboProductos .Text = registro["distribuidora"].ToString();
+                txtPrecio.Text = registro["precio"].ToString();
+                txtUnidades.Text = registro["cantidad"].ToString();
+
+            }
+            conexion.Close();
+
+
+        }
+    }
+}
